c91eec639b536e374188ce1c0651368152c3081b
Don Smith <don@antiantonym.com>
2017-08-26 09:33:34 +1200

    remove event list additions

--- client/actions/events.js
+++ client/actions/events.js
@@ -3,8 +3,6 @@ import {showError, clearError} from './error'
 
 export const REQUEST_ADD_EVENT = 'REQUEST_ADD_EVENT'
 export const RECEIVE_ADD_EVENT = 'RECEIVE_ADD_EVENT'
-export const REQUEST_EVENT_LIST = 'REQUEST_EVENT_LIST'
-export const RECEIVE_EVENT_LIST = 'RECEIVE_EVENT_LIST'
 
 const requestAddEvent = () => {
   return {
@@ -18,39 +16,12 @@ const receiveAddEvent = () => {
   }
 }
 
-const requestEventList = () => {
-  return {
-    type: REQUEST_EVENT_LIST
-  }
-}
-
-const receiveEventList = (events) => {
-  return {
-    type: RECEIVE_EVENT_LIST,
-    events
-  }
-}
-
 export function addNewEvent (event) {
   return (dispatch) => {
     dispatch(requestAddEvent())
     request('post', '/events', event)
       .then(res => {
         dispatch(receiveAddEvent())
-        dispatch(clearError())
-      })
-      .catch(() => {
-        dispatch(showError('An unexpected error has occurred.'))
-      })
-  }
-}
-
-export function getEventList () {
-  return (dispatch) => {
-    dispatch(requestEventList())
-    request('get', '/events')
-      .then(res => {
-        dispatch(receiveEventList(res.body))
         dispatch(clearError())
       })
       .catch(() => {
