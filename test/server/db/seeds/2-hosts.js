faf5ebeb2613b9fe1f80e0d62ce84cba689204a2
Don Smith <don@antiantonym.com>
2017-09-04 01:42:11 +1200

    remove the host entity

--- test/server/db/seeds/2-hosts.js
+++
@@ -1,5 +0,0 @@
-exports.seed = (knex, Promise) => {
-  return knex('hosts').insert([
-    {id: 1, user_id: 1, name: 'Julie'}
-  ])
-}
--- test/server/db/seeds/3-events.js
+++
@@ -1,6 +0,0 @@
-exports.seed = function (knex, Promise) {
-  return knex('events').insert([
-    {id: 1, host_id: 1, location: '123 Happy Lane', offering_type: 'wine'},
-    {id: 2, host_id: 1, location: '123 Yum Drive', offering_type: 'beer'}
-  ])
-}
