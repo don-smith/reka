fd2e481cfd669802d78d5e2e40a68a58176ac5d8
Don Smith <don@antiantonym.com>
2017-09-15 21:12:41 +1200

    get first ui test in place

--- test/client/components/App.test.js
+++ test/client/components/App.test.js
@@ -1,11 +1,12 @@
+import 'ignore-styles'
 import test from 'ava'
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import App from '../../client/components/App'
+import App from '../../../client/components/App'
 
-test('<App />', t => {
-  const expected = 'React development has begun!'
+test('<App /> contains the router', t => {
+  const expected = '<BrowserRouter />'
   const wrapper = shallow(<App />)
   t.is(wrapper.text(), expected)
 })
