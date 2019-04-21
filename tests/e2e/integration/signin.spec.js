3f60ea19e70ed6bde8496797b6f3a98a505453b9
Don Smith <don@antiantonym.com>
2019-04-21 14:10:04 +1200

    :file_folder: test :arrow_right: tests

--- tests/e2e/integration/signin.spec.js
+++ tests/e2e/integration/signin.spec.js
@@ -1,8 +1,8 @@
 describe('The Signin page', () => {
   it('can be logged into', () => {
     cy.visit('/signin')
-    cy.get('[data-e2e="username"]').type('jules')
-    cy.get('[data-e2e="password"').type('jules')
+    cy.get('[data-e2e="username"] input').type('jules')
+    cy.get('[data-e2e="password"] input').type('jules')
     cy.get('[data-e2e="sign-in-button"]').click()
     cy.url().should('include', '/events')
   })
